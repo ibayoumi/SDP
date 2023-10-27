ef863c5f575739de242add6e5f6c5106a735c8e5
citlalr3 <99466352+citlalr3@users.noreply.github.com>
2023-10-27 11:31:57 -0700

    csv_logger: added labels

--- csv_logger.py
+++ csv_logger.py
@@ -1,62 +1,42 @@
-# import pyxdf
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# # Second test: Respiration AND Heart Rate
-# #data, header = pyxdf.load_xdf('C:\\Users\\citla\\SeniorDesign\\App-Zephyr-main\\sub-P001\\ses-S001\\eeg\\sub-P001_ses-S001_task-Default_run-001_eeg.xdf')
-# # First test: Respiration Rate
-# #data, header = pyxdf.load_xdf('C:\\Users\\citla\\SeniorDesign\\App-Zephyr-main\\sub-P001\\ses-S001\\eeg\\sub-P001_ses-S001_task-Default_run-003_eeg.xdf')
-# # Third test: Heart rate while watching Analog Horror
-# #data, header = pyxdf.load_xdf('C:\\Users\\citla\\SeniorDesign\\App-Zephyr-main\\sub-P001\\ses-S001\\eeg\\sub-P001_ses-S001_task-Default[_acq-]_run-001_eeg.xdf')
-# data, header = pyxdf.load_xdf('C:\\Users\\citla\\SeniorDesign\\App-Zephyr-main\\sub-P001\\ses-S001\\eeg\\sub-P001_ses-S001_task-Default_run-007_eeg.xdf')
-
-# for stream in data:
-#     y = stream['time_series']
-
-#     if isinstance(y, list):
-#         # list of strings, draw one vertical line for each marker
-#         for timestamp, marker in zip(stream['time_stamps'], y):
-#             plt.axvline(x=timestamp)
-#             print(f'Marker "{marker[0]}" @ {timestamp:.2f}s')
-#     elif isinstance(y, np.ndarray):
-#         # numeric data, draw as lines
-#         print(y)
-#         plt.plot(stream['time_stamps'], y)
-#     else:
-#         raise RuntimeError('Unknown stream format')
-
-# plt.show()
 
 # Trying the pylsl example now to *hopefully* get real-time data
 from pylsl import StreamInlet, resolve_stream
 
 
 def main():
-    # first resolve an EEG stream on the lab network
-    print("TEST: idk where this thing is stuck ;v;")
-    print("looking for an EEG stream...")
-    #streams = resolve_stream('type', 'EEG')
-    streams = resolve_stream('name', 'ZephyrGeneral')
+    try:
+        # first resolve an EEG stream on the lab network
+        print("looking for an EEG stream...")
+        streams = resolve_stream('name', 'ZephyrGeneral')
 
-    print("PAST STREAMS")
-    # create a new inlet to read from the stream
-    inlet = StreamInlet(streams[0])
+        # create a new inlet to read from the stream
+        inlet = StreamInlet(streams[0])
 
-    print("PAST INLET")
+        # Create a new file
+        filename = input("Enter CSV file name: ")
+        file = open(f"{filename}.csv", "w")
 
-    filename = input("Enter CSV file name: ")
+        # Write labels to the file
+        file.write("Time,Timestamp,Heartrate,Breathing Rate,Skin Temperature,Posture,Activity,Peak Acceleration,Battery Voltage,Breathing Rate Amplitude,ECG Amplitude,\
+        ECG Noise,Vertical Acceleration Minimum,Vertical Acceleration Peak,Lateral Acceleration Minimum,Lateral Acceleration Peak,Sagittal Acceleration Minimum,\
+        Sagittal Acceleration Peak,\n")
 
-    while True:
-        # get a new sample (you can also omit the timestamp part if you're not
-        # interested in it)
-        sample, timestamp = inlet.pull_sample()
-        print(timestamp, sample)
+        while True:
+            # get a new sample (you can also omit the timestamp part if you're not
+            # interested in it)
+            sample, timestamp = inlet.pull_sample()
+            print(timestamp, sample)
 
-        with open(f"{filename}.csv", "a") as file:
-            for item in sample:
+            for item in sample[0:18]:
                 file.write(str(item) + ',')
             file.write('\n')
 
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt. Shutting down...")
+        file.close()
+
+    exit()
+    
 
 main()
 # if __name__ == '__main__':
