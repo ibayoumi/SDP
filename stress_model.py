9da42dd6f280b30b3d3c2fac52bef51a0acaa289
citlalr3 <99466352+citlalr3@users.noreply.github.com>
2024-02-15 11:34:54 -0800

    Changed stress model to Decision Tree

--- stress_model.py
+++ stress_model.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score, zero_one_loss
 from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 # BIOHARNESS/BIOMETRICS
 from pylsl import StreamInlet, resolve_stream, resolve_streams
@@ -69,7 +70,7 @@ def get_biometrics(gen_inlet, rr_inlet, show_window=False, show_result=False):
         print("RR Window: ", rr_window)
 
     # Calculate sdnn, rmssd
-    sdnn = pyhrv.time_domain.sdnn(rr_window)['sdnn'] * (10**-2)
+    sdnn = pyhrv.time_domain.sdnn(rr_window)['sdnn'] * (10**-2)  # unsure of this conversion...
     rmssd = pyhrv.time_domain.rmssd(rr_window)['rmssd'] * (10**-2)
     mean_hr = np.mean(hr_window)
 
@@ -83,7 +84,7 @@ def get_biometrics(gen_inlet, rr_inlet, show_window=False, show_result=False):
 
 
 # Read in data and convert to numpy array
-data = pd.read_csv("C:\\Users\\citla\\SeniorDesign\\Datasets\\external_data\\MIT_driver_stress\\extracted_stress_data.csv")
+data = pd.read_csv("C:\\Users\\citla\\SeniorDesign\\Datasets\\external_data\\MIT_driver_stress\\extracted_stress_data_no_ecg.csv")
 data = data.to_numpy()
 
 # first resolve an EEG stream on the lab network
@@ -106,39 +107,38 @@ rr_inlet = StreamInlet(rrStream)
 # Train ML Model
 
 # Data Splits
-X, y = data[:,:3], data[:,-1]
+# Split into X and Y
+X, y = data[:,:3], data[:,3]
 X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size=0.2, random_state=seed, shuffle=True)
 
-# Create and train classifier
-mlp_classifier = MLPClassifier(hidden_layer_sizes = (100,10,400), activation='relu', solver='sgd', learning_rate='constant', learning_rate_init=0.001, momentum=0.1, batch_size=30, max_iter=300)
-mlp_classifier.fit(X_tr, y_tr)
+# Initialize the model
+dt = DecisionTreeClassifier(criterion='entropy', max_depth=15, min_samples_split=200, min_samples_leaf=50)
 
-# Compute the training and test error rates
-y_tr_pred = mlp_classifier.predict(X_tr)
-tr_accuracy = accuracy_score(y_tr, y_tr_pred)
-tr_error = 1 - (tr_accuracy)
+# Fit the model to the training set
+dt.fit(X_tr, y_tr)
 
-y_val_pred = mlp_classifier.predict(X_val)
-val_accuracy = accuracy_score(y_val, y_val_pred)
-val_error = 1 - (val_accuracy)
+# Compute the training and test errors
+y_tr_pred = dt.predict(X_tr)
+train_error = 1 - (accuracy_score(y_tr, y_tr_pred))
+train_error = 1 - (accuracy_score(y_tr, y_tr_pred))
 
-print("Training Error: ", tr_error)
-print("Validation Error: ", val_error)
+y_val_pred = dt.predict(X_val)
+val_error = 1 - (accuracy_score(y_val, y_val_pred))
 
-print("Training Accuracy: ", tr_accuracy)
-print("Validation Accuracy: ", val_accuracy)
+print("Training Error: ", train_error)
+print("Testing Error: ", val_error)
 
 # Pull samples from BioHarness and predict human state using ML Model
 # TODO: While loop that continuously pulls samples and predicts
 while(True):
     X_te = get_biometrics(general_inlet, rr_inlet, show_window=True, show_result=True)
 
-    y_te_pred = mlp_classifier.predict(X_te.reshape(1,-1))  # Expects 2D array. "Reshape your data using array.reshape(1,-1) if it contains a single sample"
+    y_te_pred = dt.predict(X_te.reshape(1,-1))  # Expects 2D array. "Reshape your data using array.reshape(1,-1) if it contains a single sample"
 
     # Confidence Score
-    conf = mlp_classifier.predict_proba(X_te.reshape(1,-1))
+    conf = dt.predict_proba(X_te.reshape(1,-1))
     print(conf)
-    print(mlp_classifier.classes_)
+    print(dt.classes_)
 
     # TODO: Send data to CARLA (create function/module for this)
 
